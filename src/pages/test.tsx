b1fa641d009a5e1a84a4d30375100ae8b9ffb2bd
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-07-09 17:55:14 +0530

    added test page

---
+++ src/pages/test.tsx
@@ -0,0 +1,25 @@
+import { Box, Heading } from '@chakra-ui/react';
+import { NextPage } from 'next';
+// import Navbar from '../ui/Navbar/Nav';
+import AppLayout from "../components/AppLayout"
+import App from 'next/app';
+
+const Test: NextPage = () => {
+  return (
+    <Box>
+      {/* <Navbar /> */}
+      <AppLayout>
+      <Box padding="4">
+        <Heading size="lg">Welcome to the 'Test' Page!</Heading>
+        {/* Add your desired content here */}
+      </Box>
+    <ApolloProvider client={client}>
+      {/* Your app components */}
+    </ApolloProvider>
+      </AppLayout>  
+    </Box>
+  );
+};
+
+export default Test;
+
